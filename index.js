51e6498f9bf13e6242dc2c86c92ca343fb1a208b
Vignesh-2711 <vigneshrao282@gmail.com>
2025-02-08 15:19:19 +0530

    fixed error

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose');
-const Student = require('./model/student');
-const studentrouter = require('./Routes/studentRoutes')
-const courserouter = require('./Routes/courseRoutes');
-const enrollmentrouter = require('./Routes/enrollmentRoutes');
-const attendancerouter = require('./Routes/attendanceRoutes')
+    const express = require('express')
+    const app = express()
+    const mongoose = require('mongoose');
+    const Student = require('./model/student');
+    const studentrouter = require('./Routes/studentRoutes')
+    const courserouter = require('./Routes/courseRoutes');
+    const enrollmentrouter = require('./Routes/enrollmentRoutes');
+    const attendancerouter = require('./Routes/attendanceRoutes')
 
 
-app.listen(5000,()=>{
-    console.log('server running on 5000');
-    
-})
-
-app.get('/',(req,res)=>{
-    res.send('server started..')
-})
-
-app.use(express.json())
-
-mongoose.connect('mongodb+srv://vigneshrao:Vignesh@mycluster.0p0cv.mongodb.net/?retryWrites=true&w=majority&appName=myCluster')
-    .then(()=>{console.log('DB connected..');
-    })
-    .catch((err)=>{console.log(err);
+    app.listen(5000,()=>{
+        console.log('server running on 5000');
+        
     })
 
-app.use("",studentrouter)
-app.use("",courserouter)
-app.use("",enrollmentrouter)
-app.use("",attendancerouter)
+    app.get('/',(req,res)=>{
+        res.send('server started..')
+    })
+
+    app.use(express.json())
+
+    mongoose.connect('mongodb+srv://vigneshrao:Vignesh@mycluster.0p0cv.mongodb.net/studentsDB?retryWrites=true&w=majority&appName=myCluster')
+        .then(()=>{console.log('DB connected..');
+        })
+        .catch((err)=>{console.log(err);
+        })
+
+    app.use("",studentrouter)
+    app.use("",courserouter)
+    app.use("",enrollmentrouter)
+    app.use("",attendancerouter)
